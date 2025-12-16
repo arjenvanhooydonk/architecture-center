4e0693d2f87679ee3b15ebd327294745f6e5c9c3
paasapcom <paa@sap.com>
2025-12-16 21:37:19 +0000

    pr build ➝ gh pages

---
+++ @preview/jmryan5-RA0027-test/assets/js/acecf23e.1e21e82b.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/jmryan5-RA0027-test/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/jmryan5-RA0027-test/blog/authors"}')}}]);
