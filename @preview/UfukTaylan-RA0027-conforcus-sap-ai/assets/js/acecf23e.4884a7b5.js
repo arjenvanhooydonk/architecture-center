78bdc309d9cb8e78167c1c914120d77a28b426b9
paasapcom <paa@sap.com>
2025-12-19 09:42:36 +0000

    cleanup: removed folder @preview/UfukTaylan-RA0027-conforcus-sap-ai after merge

--- @preview/UfukTaylan-RA0027-conforcus-sap-ai/assets/js/acecf23e.4884a7b5.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/UfukTaylan-RA0027-conforcus-sap-ai/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/UfukTaylan-RA0027-conforcus-sap-ai/blog/authors"}')}}]);
