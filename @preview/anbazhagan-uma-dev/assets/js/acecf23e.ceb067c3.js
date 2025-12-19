bf1c628c23ac25c3c84af25cb970699b4df60699
paasapcom <paa@sap.com>
2025-12-19 09:43:55 +0000

    cleanup: removed folder @preview/anbazhagan-uma-dev after merge

--- @preview/anbazhagan-uma-dev/assets/js/acecf23e.ceb067c3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/anbazhagan-uma-dev/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/anbazhagan-uma-dev/blog/authors"}')}}]);
