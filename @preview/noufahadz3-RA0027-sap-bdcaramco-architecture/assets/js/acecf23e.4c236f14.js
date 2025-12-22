62b891bbd9eb07bbe33ce2d48d6a1961229941c7
paasapcom <paa@sap.com>
2025-12-22 18:27:31 +0000

    pr build ➝ gh pages

---
+++ @preview/noufahadz3-RA0027-sap-bdcaramco-architecture/assets/js/acecf23e.4c236f14.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/noufahadz3-RA0027-sap-bdcaramco-architecture/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/noufahadz3-RA0027-sap-bdcaramco-architecture/blog/authors"}')}}]);
