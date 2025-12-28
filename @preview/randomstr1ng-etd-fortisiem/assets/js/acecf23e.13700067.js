97fd87378a3c1451d58703be31fa57720dd02168
paasapcom <paa@sap.com>
2025-12-28 09:20:44 +0000

    pr build ➝ gh pages

---
+++ @preview/randomstr1ng-etd-fortisiem/assets/js/acecf23e.13700067.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/randomstr1ng-etd-fortisiem/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/randomstr1ng-etd-fortisiem/blog/authors"}')}}]);
