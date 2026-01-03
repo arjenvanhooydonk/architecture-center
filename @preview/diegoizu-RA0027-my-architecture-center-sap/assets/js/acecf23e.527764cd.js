c635e6570f9d11c41584570b907da70571dfe138
paasapcom <paa@sap.com>
2026-01-03 14:16:04 +0000

    cleanup: removed folder @preview/diegoizu-RA0027-my-architecture-center-sap after merge

--- @preview/diegoizu-RA0027-my-architecture-center-sap/assets/js/acecf23e.527764cd.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/diegoizu-RA0027-my-architecture-center-sap/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/diegoizu-RA0027-my-architecture-center-sap/blog/authors"}')}}]);
