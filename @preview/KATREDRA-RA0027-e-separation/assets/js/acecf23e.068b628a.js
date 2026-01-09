4b20f6cc5d9886066f0302051294e20cff66ad3a
paasapcom <paa@sap.com>
2026-01-09 13:06:23 +0000

    cleanup: removed folder @preview/KATREDRA-RA0027-e-separation after merge

--- @preview/KATREDRA-RA0027-e-separation/assets/js/acecf23e.068b628a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/KATREDRA-RA0027-e-separation/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/KATREDRA-RA0027-e-separation/blog/authors"}')}}]);
