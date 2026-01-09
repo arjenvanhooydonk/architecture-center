23ecc5857dc910e537fda86ad27f573ff0812d1a
paasapcom <paa@sap.com>
2026-01-09 13:05:51 +0000

    cleanup: removed folder @preview/sangnahs-RA0027-test after merge

--- @preview/sangnahs-RA0027-test/assets/js/acecf23e.8a5cb715.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/sangnahs-RA0027-test/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/sangnahs-RA0027-test/blog/authors"}')}}]);
