afe4615a58e2e7445c2c6e44c5f87f7618cebc3f
paasapcom <paa@sap.com>
2026-01-12 13:11:59 +0000

    pr build ➝ gh pages

---
+++ @preview/navyakhurana-sitemap-datetime/assets/js/acecf23e.5f612583.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/navyakhurana-sitemap-datetime/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/navyakhurana-sitemap-datetime/blog/authors"}')}}]);
