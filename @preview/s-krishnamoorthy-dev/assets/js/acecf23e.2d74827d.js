8baba924f21d7e5654aa0b705f2ba2af3123cc63
paasapcom <paa@sap.com>
2026-01-13 00:42:00 +0000

    pr build ➝ gh pages

---
+++ @preview/s-krishnamoorthy-dev/assets/js/acecf23e.2d74827d.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/s-krishnamoorthy-dev/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/s-krishnamoorthy-dev/blog/authors"}')}}]);
