1b69b417fff942a29e575563c6a6993d5d6e0a06
paasapcom <paa@sap.com>
2026-01-27 15:37:18 +0000

    pr build ➝ gh pages

---
+++ @preview/SreekySAP-RA0027-architecture-advisor/assets/js/acecf23e.c482aa77.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/SreekySAP-RA0027-architecture-advisor/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/SreekySAP-RA0027-architecture-advisor/blog/authors"}')}}]);
