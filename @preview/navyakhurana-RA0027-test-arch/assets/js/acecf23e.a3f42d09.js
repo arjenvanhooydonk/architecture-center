6766ef0b6d859cae93ccca51832a7399223ceaeb
Navya Khurana <navya.khurana@sap.com>
2026-02-02 09:24:35 +0530

    Delete @preview/navyakhurana-RA0027-test-arch directory

--- @preview/navyakhurana-RA0027-test-arch/assets/js/acecf23e.a3f42d09.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/navyakhurana-RA0027-test-arch/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/navyakhurana-RA0027-test-arch/blog/authors"}')}}]);
