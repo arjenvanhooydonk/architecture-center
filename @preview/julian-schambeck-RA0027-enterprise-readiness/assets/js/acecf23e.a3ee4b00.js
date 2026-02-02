94c1480f49d3725036be8a2e8241e1967f1195f5
Navya Khurana <navya.khurana@sap.com>
2026-02-02 09:24:56 +0530

    Delete @preview/julian-schambeck-RA0027-enterprise-readiness directory

--- @preview/julian-schambeck-RA0027-enterprise-readiness/assets/js/acecf23e.a3ee4b00.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/julian-schambeck-RA0027-enterprise-readiness/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/julian-schambeck-RA0027-enterprise-readiness/blog/authors"}')}}]);
