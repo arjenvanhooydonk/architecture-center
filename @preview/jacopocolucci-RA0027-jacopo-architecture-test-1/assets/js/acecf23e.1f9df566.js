fc8e15fc1b394df634744f897a6e3f069b31eb10
Navya Khurana <navya.khurana@sap.com>
2026-02-02 09:25:18 +0530

    Delete @preview/jacopocolucci-RA0027-jacopo-architecture-test-1 directory

--- @preview/jacopocolucci-RA0027-jacopo-architecture-test-1/assets/js/acecf23e.1f9df566.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/jacopocolucci-RA0027-jacopo-architecture-test-1/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/jacopocolucci-RA0027-jacopo-architecture-test-1/blog/authors"}')}}]);
