a332d137dab3eff1c1dd40bd247506fa730ba306
Navya Khurana <navya.khurana@sap.com>
2026-02-02 09:24:20 +0530

    Delete @preview/navyakhurana-sitemap-datetime directory

--- @preview/navyakhurana-sitemap-datetime/assets/js/acecf23e.5f612583.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/navyakhurana-sitemap-datetime/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/navyakhurana-sitemap-datetime/blog/authors"}')}}]);
