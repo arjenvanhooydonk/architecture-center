46b749040216848667c848c73c41120eb753b611
paasapcom <paa@sap.com>
2026-02-03 16:29:17 +0000

    pr build ➝ gh pages

---
+++ @preview/johnvinhhub-RA0028-sap-technical-architecture/assets/js/acecf23e.86dc8189.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/johnvinhhub-RA0028-sap-technical-architecture/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/johnvinhhub-RA0028-sap-technical-architecture/blog/authors"}')}}]);
