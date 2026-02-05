2cadbe97e7b650d88fd9bc9da43e19e72022f755
Pierre-Olivier Basseville <pierre-olivier.basseville@sap.com>
2026-02-05 12:59:05 +0100

    Delete @preview/jensalbrecht-agilita-RA0028-dahlinger directory

--- @preview/jensalbrecht-agilita-RA0028-dahlinger/assets/js/acecf23e.34fb19d0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/jensalbrecht-agilita-RA0028-dahlinger/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/jensalbrecht-agilita-RA0028-dahlinger/blog/authors"}')}}]);
