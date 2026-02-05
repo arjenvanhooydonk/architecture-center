6e9163ef1c1ee32057f66d7b5a4353f7c2b598c7
paasapcom <paa@sap.com>
2026-02-05 18:14:22 +0000

    pr build ➝ gh pages

---
+++ @preview/riskartika-RA0028-mr/assets/js/acecf23e.40fcb4fd.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/riskartika-RA0028-mr/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/riskartika-RA0028-mr/blog/authors"}')}}]);
