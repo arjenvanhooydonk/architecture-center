ff0318bee5c9022b3781b4f7a82d07ed63043159
Pierre-Olivier Basseville <pierre-olivier.basseville@sap.com>
2026-02-05 12:59:29 +0100

    Delete @preview/konchits-RA0028-test-solution directory

--- @preview/konchits-RA0028-test-solution/assets/js/acecf23e.14079083.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/konchits-RA0028-test-solution/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/konchits-RA0028-test-solution/blog/authors"}')}}]);
