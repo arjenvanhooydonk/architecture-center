6e39d9d15078456f9f072d58a965f4b4c9179e6d
paasapcom <paa@sap.com>
2026-02-05 11:54:39 +0000

    cleanup: removed folder @preview/johnvinhhub-RA0028-sap-technical-architecture after merge

--- @preview/johnvinhhub-RA0028-sap-technical-architecture/assets/js/acecf23e.86dc8189.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/johnvinhhub-RA0028-sap-technical-architecture/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/johnvinhhub-RA0028-sap-technical-architecture/blog/authors"}')}}]);
