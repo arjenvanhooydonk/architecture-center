b601468f89333555de5a0ead516acc8d46fdb94e
paasapcom <paa@sap.com>
2026-02-11 13:14:01 +0000

    pr build ➝ gh pages

---
+++ @preview/ivanajaklitsch-RA0028-internalprojects/assets/js/acecf23e.85833b73.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912(e){e.exports=JSON.parse('{"blogBasePath":"/@preview/ivanajaklitsch-RA0028-internalprojects/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/ivanajaklitsch-RA0028-internalprojects/blog/authors"}')}}]);
