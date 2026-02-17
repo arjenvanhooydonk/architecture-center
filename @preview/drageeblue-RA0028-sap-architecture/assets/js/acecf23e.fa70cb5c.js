f11a7dbdddd9be278c802b967536092807b885ac
paasapcom <paa@sap.com>
2026-02-17 15:12:52 +0000

    pr build ➝ gh pages

---
+++ @preview/drageeblue-RA0028-sap-architecture/assets/js/acecf23e.fa70cb5c.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912(e){e.exports=JSON.parse('{"blogBasePath":"/@preview/drageeblue-RA0028-sap-architecture/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/drageeblue-RA0028-sap-architecture/blog/authors"}')}}]);
