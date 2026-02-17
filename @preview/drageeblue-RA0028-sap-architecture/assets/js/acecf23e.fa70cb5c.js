3e406310a161edd3569106e95112007a0608c274
paasapcom <paa@sap.com>
2026-02-17 15:19:32 +0000

    cleanup: removed folder @preview/drageeblue-RA0028-sap-architecture after merge

--- @preview/drageeblue-RA0028-sap-architecture/assets/js/acecf23e.fa70cb5c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912(e){e.exports=JSON.parse('{"blogBasePath":"/@preview/drageeblue-RA0028-sap-architecture/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/drageeblue-RA0028-sap-architecture/blog/authors"}')}}]);
